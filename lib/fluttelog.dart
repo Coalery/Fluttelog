4d377675b6e4cd855df85b0715a3d7130e802b22
러리 <doralife12@naver.com>
2021-07-26 01:35:56 +0900

    Feat: Test build

--- lib/fluttelog.dart
+++ lib/fluttelog.dart
@@ -1,18 +1,11 @@
-// You have generated a new plugin project without
-// specifying the `--platforms` flag. A plugin project supports no platforms is generated.
-// To add platforms, run `flutter create -t plugin --platforms <platforms> .` under the same
-// directory. You can also find a detailed instruction on how to add platforms in the `pubspec.yaml` at https://flutter.dev/docs/development/packages-and-plugins/developing-packages#plugin-platforms.
+import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-
-class Fluttelog {
-  static const MethodChannel _channel =
-      const MethodChannel('fluttelog');
-
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+class FluttelogMarkDown extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Markdown(
+      data: '## 안녕!',
+    );
   }
 }
